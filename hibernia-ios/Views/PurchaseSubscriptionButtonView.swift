946868ddca4d55ead8b6d161d270c42971825ceb
Oliver Bevan <oliver@volani.co.uk>
2022-10-14 03:20:42 +0100

    Trial adding link to website for privacy purposes.

--- hibernia-ios/Views/PurchaseSubscriptionButtonView.swift
+++ hibernia-ios/Views/PurchaseSubscriptionButtonView.swift
@@ -11,28 +11,36 @@ struct PurchaseSubscriptionButtonView: View {
     @EnvironmentObject var subscriptionService: IAPSubscriptionService
     
     var body: some View {
-        ZStack {
-            Button {
-                Task {
-                    if subscriptionService.processing != true {
-                        await subscriptionService.setSubscriptionProduct()
-                        await subscriptionService.subscribe()
+        VStack {
+            ZStack {
+                Button {
+                    Task {
+                        if subscriptionService.processing != true {
+                            await subscriptionService.setSubscriptionProduct()
+                            await subscriptionService.subscribe()
+                        }
+                    }
+                } label: {
+                    HStack(alignment: .bottom) {
+                        Image(systemName: "wand.and.stars")
+                            .foregroundColor(.highlightStart)
+                        Text("Subscribe to Connect")
+                            .font(.custom("Comfortaa", size: 16))
+                            .bold()
+                            .foregroundColor(.highlightStart)
                     }
                 }
-            } label: {
-                HStack(alignment: .bottom) {
-                    Image(systemName: "wand.and.stars")
-                        .foregroundColor(.highlightStart)
-                    Text("Subscribe to Connect").font(.custom("Comfortaa", size: 16))
-                        .bold()
-                        .foregroundColor(.highlightStart)
+                .buttonStyle(NeumorphicButtonStyle())
+                .disabled(subscriptionService.processing)
+                if subscriptionService.processing {
+                    ProgressView()
                 }
             }
-            .buttonStyle(NeumorphicButtonStyle())
-            .disabled(subscriptionService.processing)
-            if subscriptionService.processing {
-                ProgressView()
-            }
+            
+            Text("[Remember, HiberniaVPN collects no data.](https://hiberniavpn.com)")
+                .font(.custom("Comfortaa", size: 12))
+                .foregroundColor(.highlightStart)
+                .padding()
         }
     }
 }
